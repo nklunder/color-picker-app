8255f7d3242073dfec412d05835172ab4a10dd3e
Nick Klunder <Wilson@Mikes-iMac.local>
2015-02-16 15:13:43 -0800

    add checkerboard background to color square

--- js/app.js
+++ js/app.js
@@ -12,25 +12,27 @@ var colors = {
   a: sliders.alpha.value
 };
 
+var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
 var colorSquare = document.getElementById('color-square');
+  colorSquare.setAttribute("style", "background:" + computedColor); 
 
 sliders.hue.oninput = function () {
   colors.h = sliders.hue.value;
-  var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+  computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
   colorSquare.setAttribute("style", "background:" + computedColor);
 };
 sliders.saturation.oninput = function () {
   colors.s = sliders.saturation.value;
-  var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+  computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
   colorSquare.setAttribute("style", "background:" + computedColor);
 };
 sliders.lightness.oninput = function () {
   colors.l = sliders.lightness.value;
-  var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+  computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
   colorSquare.setAttribute("style", "background:" + computedColor);
 };
 sliders.alpha.oninput = function () {
   colors.a = sliders.alpha.value;
-  var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+  computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
   colorSquare.setAttribute("style", "background:" + computedColor);
 };
