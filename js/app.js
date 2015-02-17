9a35e40836fc9e266f441382c6a73d0fe86c6d66
Nicholas Klunder <nklunder@gmail.com>
2015-02-16 20:29:15 -0800

    fix a couple of minor syntax issues

--- js/app.js
+++ js/app.js
@@ -12,16 +12,16 @@ var colors = {
   a: sliders.alpha.value
 };
 
+var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+var colorSquare = document.getElementById('color-square');
+  colorSquare.setAttribute("style", "background:" + computedColor);
+
 function updateValue() {
   var val = this.name.charAt(0);
   colors[val] = sliders[this.name].value;
   computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
   colorSquare.setAttribute("style", "background:" + computedColor);
 }
-
-var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
-var colorSquare = document.getElementById('color-square');
-  colorSquare.setAttribute("style", "background:" + computedColor);
 
 sliders.hue.oninput = updateValue;
 sliders.saturation.oninput = updateValue;
