803caf5d2accd58f5de838a16d8ccbe112da3ad3
Nicholas Klunder <nklunder@gmail.com>
2015-02-22 14:25:04 -0800

    add hsl to rgb functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var sliders = {
-  hue:        document.getElementById('hue-slider'),
+  hue: document.getElementById('hue-slider'),
   saturation: document.getElementById('saturation-slider'),
-  lightness:  document.getElementById('lightness-slider'),
-  alpha:      document.getElementById('alpha-slider')
+  lightness: document.getElementById('lightness-slider'),
+  alpha: document.getElementById('alpha-slider')
 };
 
 var colors = {
@@ -19,7 +19,7 @@ var numberVals = {
   a: document.getElementById('a-val'),
 }
 
-var computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
+var hsla = "hsla(" + colors.h + "," + colors.s + "%," + colors.l + "%," + colors.a + ")";
 var colorSquare = document.getElementById('color-square');
 
 
@@ -27,12 +27,55 @@ function updateValue() {
   var val = this.name.charAt(0);
   colors[val] = sliders[this.name].value;
   numberVals[val].textContent = colors[val];
-  computedColor = "hsla(" + colors.h + "," + colors.s + "%," + colors.l +"%," + colors.a + ")";
-  colorSquare.setAttribute("style", "background:" + computedColor);
+  rgba = hsl2rgb(colors.h, colors.s, colors.l);
+  hsla = "hsla(" + colors.h + "," + colors.s + "%," + colors.l + "%," + colors.a + ")";
+  rgba = "rgba(" + colors.h + "," + colors.s + "," + colors.l + "," + colors.a + ")";
+  colorSquare.setAttribute("style", "background:" + hsla);
+}
+
+function hsl2rgb(h, s, l) {
+  var m1, m2, hue,
+      r, g, b;
+
+  s /= 100;
+  l /= 100;
+  if (s === 0) {
+    r = g = b = (l * 255);
+  } else {
+    if (l <= 0.5) {
+      m2 = l * (s + 1);
+    } else {
+      m2 = l + s - l * s;
+    }
+    m1 = l * 2 - m2;
+    hue = h / 360;
+    colors.r = Math.round(hue2rgb(m1, m2, hue + 1 / 3));
+    colors.g = Math.round(hue2rgb(m1, m2, hue));
+    colors.b = Math.round(hue2rgb(m1, m2, hue - 1 / 3));
+  }
+}
+
+function hue2rgb(m1, m2, hue) {
+  var v;
+  if (hue < 0)
+    hue += 1;
+  else if (hue > 1)
+    hue -= 1;
+
+  if (6 * hue < 1)
+    v = m1 + (m2 - m1) * hue * 6;
+  else if (2 * hue < 1)
+    v = m2;
+  else if (3 * hue < 2)
+    v = m1 + (m2 - m1) * (2 / 3 - hue) * 6;
+  else
+    v = m1;
+
+  return 255 * v;
 }
 
 function init() {
-  colorSquare.setAttribute("style", "background:" + computedColor);
+  colorSquare.setAttribute("style", "background:" + hsla);
 
   numberVals.h.textContent = colors.h;
   numberVals.s.textContent = colors.s;
@@ -45,17 +88,17 @@ sliders.saturation.oninput = updateValue;
 sliders.lightness.oninput = updateValue;
 sliders.alpha.oninput = updateValue;
 
-sliders.hue.onchange = function () {
-  document.getElementById('css-vals').textContent = computedColor;
+sliders.hue.onchange = function() {
+  document.getElementById('css-vals').textContent = hsla + rgba;
 };
-sliders.saturation.onchange = function () {
-  document.getElementById('css-vals').textContent = computedColor;
+sliders.saturation.onchange = function() {
+  document.getElementById('css-vals').textContent = hsla + rgba;
 };
-sliders.lightness.onchange = function () {
-  document.getElementById('css-vals').textContent = computedColor;
+sliders.lightness.onchange = function() {
+  document.getElementById('css-vals').textContent = hsla + rgba;
 };
-sliders.alpha.onchange = function () {
-  document.getElementById('css-vals').textContent = computedColor;
+sliders.alpha.onchange = function() {
+  document.getElementById('css-vals').textContent = hsla + rgba;
 };
 
 init();
